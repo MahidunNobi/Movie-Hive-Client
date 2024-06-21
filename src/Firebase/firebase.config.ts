b741e529a7d463d19e64ba0a020947cf1e8e51ce
MahidunNobi <mdmahidunnobi@gmail.com>
2024-06-21 13:10:36 +0600

    API KEYS ARE SECURED

--- src/Firebase/firebase.config.ts
+++ src/Firebase/firebase.config.ts
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAQAHECjc68xwKy-R_d-3hflZs4G1ZBhVQ",
-  authDomain: "movie-hive-8e45e.firebaseapp.com",
-  projectId: "movie-hive-8e45e",
-  storageBucket: "movie-hive-8e45e.appspot.com",
-  messagingSenderId: "586927689067",
-  appId: "1:586927689067:web:606ac7465a60ead13cd02e",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
