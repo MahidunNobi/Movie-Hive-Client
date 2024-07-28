c5b8e95696ed62ed3498f4306ec2f3bd67d6f4a6
Mahidun Nobi <mdmahidunnobi@gmail.com>
2024-07-28 11:03:31 +0600

    ADD: Footer

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../componants/SharedComponants/Navbar/Navbar";
+import Footer from "../componants/Home/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <div className="min-w-[100vw] md:min-w-[98vw] min-h-screen bg-net-black text-white relative">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
